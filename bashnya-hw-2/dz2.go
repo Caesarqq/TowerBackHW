9a2f6a082a672ff342608a6b34e5f499a130a709
Caesarqq <aszxclkh@gmail.com>
2025-11-18 00:12:07 +0300

    5

--- bashnya-hw-2/dz2.go
+++ bashnya-hw-2/dz2.go
@@ -1,38 +1,38 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func main() {
-	var name int
-	fmt.Scan(&name)
-	if name >= 12307 {
-		fmt.Println("Число не соответствует изначальному условию, попробуйте ещё раз")
-		return
-	}
-	for name < 12307 {
-		if name < 0 {
-			name *= -1
-		}
-		if name%7 == 0 {
-			name *= 39
-		}
-		if name%9 == 0 {
-			name *= 13
-			name += 1
-			continue
-		} else {
-			name += 2
-			name *= 3
-		}
+// func main() {
+// 	var name int
+// 	fmt.Scan(&name)
+// 	if name >= 12307 {
+// 		fmt.Println("Число не соответствует изначальному условию, попробуйте ещё раз")
+// 		return
+// 	}
+// 	for name < 12307 {
+// 		if name < 0 {
+// 			name *= -1
+// 		}
+// 		if name%7 == 0 {
+// 			name *= 39
+// 		}
+// 		if name%9 == 0 {
+// 			name *= 13
+// 			name += 1
+// 			continue
+// 		} else {
+// 			name += 2
+// 			name *= 3
+// 		}
 
-		if name%13 == 0 && name%9 == 0 {
-			fmt.Println("service error")
-			break
-		} else {
-			name++
-		}
-	}
-	fmt.Printf("Итоговое число: %d\n", name)
-}
+// 		if name%13 == 0 && name%9 == 0 {
+// 			fmt.Println("service error")
+// 			break
+// 		} else {
+// 			name++
+// 		}
+// 	}
+// 	fmt.Printf("Итоговое число: %d\n", name)
+// }
